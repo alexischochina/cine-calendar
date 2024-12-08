8d25a39a39cfd0c7e418380615c95f93bbf02680
Alexis Chochina <alexis@fcinq.com>
2024-12-08 19:05:50 +0100

    init swiper + début composant list item

---
+++ app/components/MovieListItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps({
+    releaseDate: {
+        type: Date,
+    },
+    poster: {
+        type: String,
+    },
+    title: {
+        type: String,
+    },
+    media: {
+        type: String,
+        default: 'unknown',
+        validator: value => ['cinema', 'streaming', 'unknown'].includes(value)
+    },
+    platform: {
+        type: String,
+        default: 'vod',
+        validator: value => ['netflix', 'primeVideo', 'disney+', 'vod'].includes(value)
+    },
+    stat: {
+        type: String,
+        default: 'unseen',
+        validator: value => ['unseen', 'seen', 'downloadAvailable'].includes(value)
+    },
+})
+</script>
+
+<template>
+    <div class="movie-list-item flex -align-center" :class="`-${props.media} -${props.platform} -${props.stat}`">
+        <div class="movie-infos flex -align-center -justify-space-between">
+            <div class="title-4">04</div>
+            <div class="title-5">Babylon</div>
+        </div>
+        <div class="stream-infos flex -align-center">
+            <Svg :name="props.media" :class="props.media"/>
+            <Svg :name="props.stat" :class="props.stat"/>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,35 @@
+<script setup>
+const containerRef = ref(null)
+const slides = ref(Array.from({ length: 10 }))
+
+const swiper = useSwiper(containerRef)
+
+onMounted(() => {
+    console.log(swiper.instance)
+})
+</script>
+
+<template>
+    <ClientOnly>
+        <MovieListItem />
+        <swiper-container ref="containerRef">
+            <swiper-slide
+                v-for="(slide, idx) in slides"
+                :key="idx"
+                style="background-color: rgb(32, 233, 70); color: white;"
+                class="title-2"
+            >
+                Slide {{ idx + 1 }}
+            </swiper-slide>
+        </swiper-container>
+    </ClientOnly>
+</template>
+
+<style lang="css">
+swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20vh;
+}
+</style>
