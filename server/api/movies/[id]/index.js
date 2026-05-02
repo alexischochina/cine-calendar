41d7e5df9a048331aec7f178ae675301b8cd1536
Alexis Chochina <alexis@fcinq.com>
2026-05-02 16:57:13 +0200

    opti cache

--- server/api/movies/[id]/index.js
+++ server/api/movies/[id]/index.js
@@ -1,5 +1,10 @@
-export default defineEventHandler(async (event) => {
+export default defineCachedEventHandler(async (event) => {
     const id = event.context.params.id;
     const config = useRuntimeConfig();
     return $fetch(`${config.apiBaseUrl}/movie/${id}?api_key=${config.apiKey}&language=fr-FR&region=FR`);
+}, {
+    maxAge: 60 * 60 * 24 * 7,
+    staleMaxAge: 60 * 60 * 24 * 30,
+    swr: true,
+    getKey: (event) => `movie:${event.context.params.id}`,
 });
--- server/api/movies/[id]/release_dates.js
+++ server/api/movies/[id]/release_dates.js
@@ -1,5 +1,10 @@
-export default defineEventHandler(async (event) => {
+export default defineCachedEventHandler(async (event) => {
     const id = event.context.params.id;
     const config = useRuntimeConfig();
     return $fetch(`${config.apiBaseUrl}/movie/${id}/release_dates?api_key=${config.apiKey}`);
+}, {
+    maxAge: 60 * 60 * 6,
+    staleMaxAge: 60 * 60 * 24,
+    swr: true,
+    getKey: (event) => `release_dates:${event.context.params.id}`,
 });
