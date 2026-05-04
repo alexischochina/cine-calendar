06e0bee31ad7ebf7e7a5a3b573075b1f2a7842bc
Alexis Chochina <alexis@fcinq.com>
2026-05-04 15:41:31 +0200

    fix date iOS Safari

--- app/composables/useMovieCalendar.js
+++ app/composables/useMovieCalendar.js
@@ -10,7 +10,7 @@ export function useMovieCalendar() {
         const year = date.getFullYear();
         const month = date.getMonth() + 1;
         const day = date.getDate();
-        return `${year}, ${month.toString().padStart(2, '0')}, ${day.toString().padStart(2, '0')}`;
+        return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
     }
 
     const getReleaseDateFromId = async (id) => {
